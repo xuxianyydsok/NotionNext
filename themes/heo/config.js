0571a7bf03c63601da4dbb57be4bad4f01de96a2
xuxianyydsok <145044505+xuxianyydsok@users.noreply.github.com>
2025-09-22 17:03:51 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://xuguochen.dpdns.org' },
-    { title: ' Welcome to My Blog', url: 'https://xuguochen.dpdns.org' }
+    { title: 'Welcome to My Blog', url: 'https://xuguochen.dpdns.org' }
   ],
 
   // 英雄区左右侧组件颠倒位置
