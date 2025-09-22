55c5d00446c4bcd119734c0c9683c319193643e5
xuxianyydsok <145044505+xuxianyydsok@users.noreply.github.com>
2025-09-22 18:52:22 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,8 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://xuguochen.dpdns.org' },
-    { title: 'Welcome to My Blog', url: 'https://xuguochen.dpdns.org' }],
+    { title: 'Welcome to My Blog', url: 'https://xuguochen.dpdns.org' }
+  ],
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
@@ -17,10 +18,10 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享生活',
-  HEO_HERO_TITLE_2: '个人成长与思维认知',
+  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'https://xuguochen.dpdns.org',
-  HEO_HERO_TITLE_4: ' Nice to meet you',
+  HEO_HERO_TITLE_4: 'Nice to Meet You',
   HEO_HERO_TITLE_5: '欢迎来到我的博客',
   HEO_HERO_TITLE_LINK: 'https://xuguochen.dpdns.org',
   // 英雄区遮罩文字
@@ -50,9 +51,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/xuxianyydsok',
+  HEO_INFO_CARD_URL2: 'https://xuguochen.dpdns.org',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://xuguochen.dpdns.org',
+  HEO_INFO_CARD_URL3: 'https://github.com/xuxianyydsok',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -115,7 +116,7 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
+  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
