5678d5b1327c60915cc90c6053fbb0a4a6bfea08
xuxianyydsok <145044505+xuxianyydsok@users.noreply.github.com>
2025-09-22 14:04:11 +0800

    Update animation.config.js

--- conf/animation.config.js
+++ conf/animation.config.js
@@ -22,7 +22,7 @@ module.exports = {
   // 樱花飘落特效
   SAKURA: process.env.NEXT_PUBLIC_SAKURA || true, // 开关
   // 漂浮线段特效
-  NEST: process.env.NEXT_PUBLIC_NEST || false, // 开关
+  NEST: process.env.NEXT_PUBLIC_NEST || true, // 开关
   // 动态彩带特效
   FLUTTERINGRIBBON: process.env.NEXT_PUBLIC_FLUTTERINGRIBBON || true, // 开关
   // 静态彩带特效
